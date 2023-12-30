8440d6299cd97eb3ceb16270f3b900d40e2c5530
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2023-12-30 17:41:40 +0530

    first commit56

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
 function CurrentTimer() {
-  const [currentHour, setCurrentHour] = useState(new Date().getHours());
-  const [currentMinute, setCurrentMinute] = useState(new Date().getMinutes());
-  const [currentSecond, setCurrentSecond] = useState(new Date().getSeconds());
-
+  const [currentTime, setCurrentTime] = useState({
+    Hours: new Date().getHours(),
+    minute: new Date().getMinutes(),
+    second: new Date().getSeconds(),
+  });
+  
   useEffect(() => {
     const interval = setInterval(() => {
-        setCurrentHour(new Date().getHours())
-        setCurrentMinute(new Date().getMinutes())
-        setCurrentSecond(new Date().getSeconds())
+        setCurrentTime({Hours: new Date().getHours(), minute: new Date().getMinutes(), second: new Date().getSeconds()})
     }, 1000)
     return () => clearInterval(interval);
   }, []);
@@ -17,14 +17,15 @@ function CurrentTimer() {
   return (
     <div className="w-full h-auto flex flex-wrap justify-evenly items-center">
     <div className="flex flex-col lg:flex-row  md:flex-row justify-around items-center  py-2 px-2 text-black timecont text-7xl bg-gradient-to-tr from-[#a619f8]/50 to-[#00f2fe]/50 rounded shadow-md shadow-black">
-      <div className=" flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentHour >= 12)?`0${(currentHour - 12)}` : currentHour}</div>
+      <div className=" flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentTime.Hours >= 12)?`0${(currentTime.Hours - 12)}` : currentTime.Hours}</div>
       <span className="md:flex md:justify-center items-center mb-6 hidden text-[120px]">:</span>
       <span className="md:justify-center md:hidden text-[70px]">••</span>
-      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentMinute < 10)?`0${currentMinute}` : currentMinute}</div>
+      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentTime.minute < 10)?`0${currentTime.minute
+      }` : currentTime.minute}</div>
       <span className="md:flex md:justify-center items-center mb-6 hidden text-[120px]">:</span>
       <span className="flex md:justify-center  md:hidden text-[70px]">••</span>
-      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentSecond < 10)? `0${currentSecond}`: currentSecond}</div>
-      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentHour >= 12)? `PM`: 'AM'}</div>
+      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentTime.second < 10)? `0${currentTime.second}`: currentTime.second}</div>
+      <div className="flex justify-center items-center w-[100%] h-[100%] py-4 px-6 lg:py-6 lg:px-6 lg:text-[90px]">{(currentTime >= 12)? `PM`: 'AM'}</div>
     </div>
     </div>
   );
